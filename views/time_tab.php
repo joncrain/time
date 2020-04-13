b8f40d33c1ade6ae464be4b3c8c847345496e21c
tuxudo <tuxudo@users.noreply.github.com>
2020-04-13 08:49:34 -0400

    Fixed yes/no logic and limited table width

--- views/time_tab.php
+++ views/time_tab.php
@@ -1,7 +1,7 @@
 <div id="time-tab"></div>
 <h2 data-i18n="time.title"></h2>
 
-<table>
+<table style="width: 550px;">
 <tr>
         <th data-i18n="time.listing.timezone">></th>
         <td id="timezone"></td>
@@ -25,14 +25,14 @@ $(document).on('appReady', function(){
     $.getJSON(appUrl + '/module/time/get_data/' + serialNumber, function(data){
         $('#timezone').text(data['timezone'])
         
-        var status=data['autotimezone']
-        status = status == 1 ? i18n.t('yes') :
-	    (status == 0 && status != '' ? i18n.t('no') : '')
-        $('#autotimezone').text(status)
+        var autotimezone=data['autotimezone']
+        autotimezone = autotimezone == 1 ? i18n.t('yes') :
+	    (autotimezone == 0 && autotimezone != null ? i18n.t('no') : '')
+        $('#autotimezone').text(autotimezone)
 
         var status=data['networktime_status']
         status = status == 1 ? i18n.t('yes') :
-	    (status == 0 && status != '' ? i18n.t('no') : '')
+	    (status == 0 && status != null ? i18n.t('no') : '')
         $('#networktime_status').text(status)
 
         $('#networktime_server').text(data['networktime_server'])
