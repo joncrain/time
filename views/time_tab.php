dac9c2b0b9cc5edd61938ae9f3408a55cd5dc10d
Jon Crain <joncrain@gmail.com>
2020-07-07 10:25:23 -0400

    Fixing time tab thanks to @tuxudo

--- views/time_tab.php
+++ views/time_tab.php
@@ -8,6 +8,10 @@ $(document).on('appReady', function(){
     $.getJSON(appUrl + '/module/time/get_data/' + serialNumber, function(data){
         var table = $('#time-tab-table');
         $.each(data, function(key,val){
+            if (key == "autotimezone" || key == "networktime_status") {
+                val = val == 1 ? i18n.t('yes') :
+         		(val == 0 && val != null ? i18n.t('no') : '')
+            }
             var th = $('<th>').text(i18n.t('time.column.' + key));
             var td = $('<td>').text(val);
             table.append($('<tr>').append(th, td));
